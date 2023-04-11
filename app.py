328780aeb24da5277608b06a3a6de487aae1dca6
MAYANK RAJ <mayank.09raj@gmail.com>
2023-04-12 01:32:43 +0530

    Diabetes pred success
    added all required html file for multiple disease prediction and Successfully integrated diabetes model

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template,request,redirect,url_for,session
 from pymongo import MongoClient
+import pandas as pd
+import numpy as np
+import pickle
+
+diab_model = pickle.load(open('diab_model.pkl', 'rb'))
 
 app = Flask(__name__)
 app.secret_key = 'super secret key'
@@ -33,6 +38,8 @@ def reg_sumbit():
     user = db.Doctors.find_one({"License": License})
     if user:
         return render_template("register.html", error="Doctor already registered")
+    if not Name or not Phone or not Email or not Speciality or not License or not Years:
+        return render_template("register.html", error="Please fill out all feild")
     db.Doctors.insert_one({"Name": Name,"Phone":Phone,"Email": Email,"Department":Speciality,"License":License,"Practice":Years}) 
     # print(Name,Phone,Email,Speciality,License,Years)
     return redirect(url_for("login"))
@@ -43,12 +50,24 @@ def log_submit():
     Name = LogDetails['name']
     License = LogDetails['licence']
     user = db.Doctors.find_one({"Name": Name,"License": License})
+    if not Name or not License :
+        return render_template("login.html", error="Please fill out all feild")
     if user:
         session['name'] = Name
         return redirect(url_for("home"))
     else:
         return redirect((url_for("login")))
     # return redirect(url_for("home"))
+
+@app.route("/feed_submit",methods=['POST'])
+def feed_submit():
+    Feedback = request.form
+    email = Feedback['email']
+    comment = Feedback['comment']
+    if not email or not comment:
+        return render_template("index.html", error="Please fill out all feild")
+    db.Feedbacks.insert_one({"Email":email,"Comments":comment})
+    return render_template("index.html", result="Thank you for your Feedback!")
 
 @app.route("/Consult")
 def consult_doc():
@@ -57,6 +76,42 @@ def consult_doc():
 @app.route("/Predict")
 def predict():
     return render_template("predict.html")
+
+@app.route("/Predict/heart")
+def pred_heart():
+    return render_template("heart.html")
+
+@app.route("/Predict/cancer")
+def pred_cancer():
+    return render_template("cancer.html")
+
+@app.route("/Predict/liver")
+def pred_liver():
+    return render_template("liver.html")
+
+@app.route("/Predict/diabetes")
+def pred_diabetes():
+    return render_template("diabetes.html")
+
+# <!-- Pregnancies,Glucose,BloodPressure,Insulin,BMI,DiabetesPedigreeFunction,Age -->
+@app.route("/Predict/diabetes/result",methods=['POST'])
+def result_diabetes():
+    result = request.form
+    Name=result['name']
+    Email=result['email']
+    Phone=result['phone']
+    Pregnancies= result['Pregnancies']
+    Glucose= result['Glucose']
+    BloodPressure=result['BloodPressure']
+    Insulin=result['Insulin']
+    BMI= result['BMI']
+    # DiabetesPedigreeFunction=result['DiabetesPedigreeFunction']
+    Age=result['Age']
+    db.Patients.insert_one({"Name":Name,"Email":Email,"Phone":Phone})
+    df=np.array([[Pregnancies,Glucose,BloodPressure,Insulin,BMI,Age]]).astype('float64')
+    output=diab_model.predict(df)
+    print(Name,Email,Phone,Pregnancies,Glucose,BloodPressure,Insulin,BMI,Age, output)
+    return redirect(url_for('pred_diabetes'))
 
 if __name__ == '__main__':
 	app.run(debug=True)
